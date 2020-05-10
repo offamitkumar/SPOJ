10deb68e807e04681cff68d8c0fe6a003b892812
Amit Kumar <offamitkumar@gmail.com>
2020-05-10 11:31:01 +0530

    simple observation

---
+++ Classical/ANARC09B.cpp
@@ -0,0 +1,45 @@
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <utility>
+#include <queue>
+#include <stack>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <map>
+#include <vector>
+#include <array>
+#include <set>
+#include <climits>
+#include <sstream>
+#include <iomanip>
+#include <cassert>
+#include <bitset>
+#include <numeric>
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree< // find_by_order & order_of_key
+        int ,
+        null_type ,
+        less<int> ,
+        rb_tree_tag ,
+        tree_order_statistics_node_update 
+> new_set;
+#define MOD 1000000007
+
+int main(void){
+#ifdef HELL_JUDGE
+    freopen("input","r",stdin);
+    freopen("output","w",stdout);
+    freopen("error","w",stderr);
+#endif 
+    long long l , w;
+    while(cin>>l>>w && l && w){
+        cout<<((l*w)/__gcd(l,w))/__gcd(l,w)<<'\n';
+    }
+    return 0;
+}
