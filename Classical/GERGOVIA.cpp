a130f7f9baed0a84e5c8d94d23f515191aebd110
Amit Kumar <offamitkumar@gmail.com>
2020-05-13 16:21:31 +0530

    greddy , O(N)

---
+++ Classical/GERGOVIA.cpp
@@ -0,0 +1,50 @@
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <utility>
+#include <queue>
+#include <stack>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <map>
+#include <vector>
+#include <array>
+#include <set>
+#include <climits>
+#include <sstream>
+#include <iomanip>
+#include <cassert>
+#include <bitset>
+#include <numeric>
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree< // find_by_order & order_of_key
+        int ,
+        null_type ,
+        less<int> ,
+        rb_tree_tag ,
+        tree_order_statistics_node_update 
+> new_set;
+#define MOD 1000000007
+
+int main(void){
+#ifdef HELL_JUDGE
+    freopen("input","r",stdin);
+    freopen("output","w",stdout);
+    freopen("error","w",stderr);
+#endif 
+    int n; 
+    while(cin>>n ,n){
+        long long int cost{} , work_remaining{};
+        for(int x,i=0;i<n;++i){
+            cin>>x; work_remaining+=(x);
+            cost+=abs(work_remaining); // remaining work either it is selling or buying , both will increase the cost
+        }
+        cout<<cost<<'\n';
+    }
+    return 0;
+}
