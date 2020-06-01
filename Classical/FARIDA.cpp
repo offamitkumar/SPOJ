e384df5d1dd2d07a8fa57f5d5c2332438afc9509
Amit Kumar <offamitkumar@gmail.com>
2020-06-01 10:45:43 +0530

    simple dp, max sum of non-adjacent elements

---
+++ Classical/FARIDA.cpp
@@ -0,0 +1,56 @@
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <utility>
+#include <queue>
+#include <stack>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <map>
+#include <unordered_map>
+#include <vector>
+#include <array>
+#include <set>
+#include <climits>
+#include <sstream>
+#include <fstream>
+#include <iomanip>
+#include <cassert>
+#include <bitset>
+#include <numeric>
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree< // find_by_order & order_of_key
+        int ,
+        null_type ,
+        less<int> ,
+        rb_tree_tag ,
+        tree_order_statistics_node_update 
+> new_set;
+#define MOD 1000000007
+
+int main(void){
+#ifdef HELL_JUDGE
+    freopen("input","r",stdin);
+    freopen("output","w",stdout);
+    freopen("error","w",stderr);
+#endif 
+    int t; scanf("%d",&t);
+    int counter{1};
+    while(t--){
+        long long n; scanf("%lld",&n);
+        long long inc{},exc{};
+        for(long long temp,x,i=0ll;i<n;++i){
+            scanf("%lld",&x);
+            temp = inc; 
+            inc = max(inc , exc+x);
+            exc = temp;
+        }
+        printf("Case %d: %lld\n",counter++,inc);
+    }
+    return 0;
+}
