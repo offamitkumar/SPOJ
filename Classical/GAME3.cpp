70f12ed21cf03cdef34703b4cfba4553e7ee54df
Amit Kumar <offamitkumar@gmail.com>
2020-06-21 22:55:28 +0530

    simple game

---
+++ Classical/GAME3.cpp
@@ -0,0 +1,146 @@
+//  Author : Amit Kumar 
+
+#include <iostream>
+#include <algorithm> 
+#include <string> 
+#include <sstream>
+#include <fstream>
+#include <iomanip> 
+#include <chrono> 
+#include <numeric> 
+#include <utility>
+#include <functional>
+
+#include <bitset>
+#include <tuple>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <array>
+#include <unordered_map>
+#include <unordered_set>
+#include <set>
+#include <map>
+#include <deque>
+#include <list>
+
+
+#include <climits>
+#include <cstring>
+#include <cmath>
+#include <cassert>
+#include <cstdio>
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace std::chrono;
+//  using namespace placeholders;
+using namespace __gnu_pbds;
+template<typename TypeInfo>
+using new_set = tree< 
+/* ob.order_of_key(element) -> number of element strictly less than element
+ * *ob.find_by_order(n) ->  nth element in increasing order
+ */
+         TypeInfo ,
+         null_type ,
+         less<TypeInfo> ,
+         rb_tree_tag ,
+         tree_order_statistics_node_update 
+ > ;
+
+void show_me() { cerr << endl; }
+ 
+template <typename HEAD, typename... TAIL>
+void show_me(HEAD H, TAIL... T) {
+  cerr << " " << (H);
+  show_me(T...);
+}
+ 
+#ifdef HELL_JUDGE
+#define show(...) cerr << "[" << #__VA_ARGS__ << "]:", show_me(__VA_ARGS__)
+#else
+#define show(...) 0
+#endif
+ 
+template<typename A, typename B>
+ostream& operator<<(ostream&out, const pair<A, B>&p){
+    out << p.first << ' ' << p.second ;
+    return out;
+}
+
+template<typename A, typename B>
+istream& operator>>(istream&in, pair<A, B>&p){
+    in >> p.first >> p.second ;
+    return in;
+}
+
+template<typename A> 
+istream& operator>>(istream&in, vector<A>&vec){
+    for(auto&itr:vec){
+        in >> itr;
+    }
+    return in;
+}
+
+template<typename A>
+ostream& operator<<(ostream&out,const vector<A>&vec){
+    for(auto&itr:vec){
+        out << itr <<' '; // VVV 
+    }
+    return out;
+}
+
+const int64_t MAXM = (int64_t)1e5+100;
+const int64_t MAXN = (int64_t)1e5+100;
+const int64_t MOD  = (int64_t)1e9+7;
+
+
+
+
+signed main(void){
+#ifdef HELL_JUDGE
+    freopen("input","r",stdin);
+    freopen("output","w",stdout);
+    freopen("error","w",stderr);
+#endif 
+    ios::sync_with_stdio(false); // FLUSH THE STREAM IF USING puts / printf / scanf/
+    cin.tie(nullptr);            //
+    cout.tie(nullptr);           //
+#ifdef HELL_JUDGE
+    auto INITIAL_TIME = high_resolution_clock::now();
+#endif 
+
+    auto check = [](int64_t x) {
+        if (x%2ll==0) return false;
+        vector<int64_t> dig;
+        while (x) {
+                dig.push_back(x%2ll);
+                x/=2ll;
+        }
+        for (int64_t i=2ll;i<(int64_t)dig.size();i+=2ll)
+                if (dig[i])
+                        return false;
+        return true;
+    };
+
+    int64_t t; cin >> t; 
+    while(t--){
+        int64_t n; cin >> n; 
+        if(check(n)){
+            cout << "Ivica" << '\n'; 
+        }else{
+            cout << "Marica" << '\n'; 
+        }
+    }
+
+
+#ifdef HELL_JUDGE
+    auto FINAL_TIME = high_resolution_clock::now();
+    cout << "Time : "
+         << duration_cast<milliseconds>(FINAL_TIME-INITIAL_TIME).count() 
+         << " ms" << '\n'; 
+#endif 
+    return 0;
+}
