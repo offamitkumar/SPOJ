060c835bf25bc95d8061abb59e56a092780f9736
Amit Kumar <offamitkumar@gmail.com>
2020-06-22 19:22:27 +0530

    Same as INVCNT

---
+++ Classical/SWAPDIFF1.cpp
@@ -0,0 +1,147 @@
+//  Author : Amit Kumar 
+
+#include <iostream>
+#include <algorithm> 
+#include <string> 
+#include <sstream>
+#include <fstream>
+#include <iomanip> 
+#include <chrono> 
+#include <numeric> 
+#include <utility>
+#include <functional>
+
+#include <bitset>
+#include <tuple>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <array>
+#include <unordered_map>
+#include <unordered_set>
+#include <set>
+#include <map>
+#include <deque>
+#include <list>
+
+
+#include <climits>
+#include <cstring>
+#include <cmath>
+#include <cassert>
+#include <cstdio>
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace std::chrono;
+//  using namespace placeholders;
+using namespace __gnu_pbds;
+template<typename TypeInfo>
+using new_set = tree< 
+/* ob.order_of_key(element) -> number of element strictly less than element
+ * *ob.find_by_order(n) ->  nth element in increasing order
+ */
+         TypeInfo ,
+         null_type ,
+         less<TypeInfo> ,
+         rb_tree_tag ,
+         tree_order_statistics_node_update 
+ > ;
+
+void show_me() { cerr << endl; }
+ 
+template <typename HEAD, typename... TAIL>
+void show_me(HEAD H, TAIL... T) {
+  cerr << " " << (H);
+  show_me(T...);
+}
+ 
+#ifdef HELL_JUDGE
+#define show(...) cerr << "[" << #__VA_ARGS__ << "]:", show_me(__VA_ARGS__)
+#else
+#define show(...) 0
+#endif
+ 
+template<typename A, typename B>
+ostream& operator<<(ostream&out, const pair<A, B>&p){
+    out << p.first << ' ' << p.second ;
+    return out;
+}
+
+template<typename A, typename B>
+istream& operator>>(istream&in, pair<A, B>&p){
+    in >> p.first >> p.second ;
+    return in;
+}
+
+template<typename A> 
+istream& operator>>(istream&in, vector<A>&vec){
+    for(auto&itr:vec){
+        in >> itr;
+    }
+    return in;
+}
+
+template<typename A>
+ostream& operator<<(ostream&out,const vector<A>&vec){
+    for(auto&itr:vec){
+        out << itr <<' '; // VVV 
+    }
+    return out;
+}
+
+const int MAXM = (int)1e5+100;
+const int MAXN = (int)1e5+100;
+const int MOD  = (int)1e9+7;
+
+array<int,MAXN>arr , temp;
+long long counter{};
+void merge(int start , int end){
+    int mid = (start+end)/2;
+    int i = start , j = mid+1 , k = start;
+    while(i<=mid && j<=end){
+        if(arr[i]>arr[j]){
+            counter+=abs(mid-i+1);
+            temp[k++] = arr[j++];
+        }else{
+            temp[k++] = arr[i++];
+        }
+    }
+    while(i<=mid){
+        temp[k++] = arr[i++];
+    }
+    while(j<=end){
+        temp[k++] = arr[j++];
+    }
+    for(i = start; i<=end; ++i){
+        arr[i] = temp[i];
+    }
+    return ;
+}
+void mergeSort(int start , int end){
+    if(start==end){return;}
+    int mid = (start+end)/2;
+    mergeSort(start , mid);
+    mergeSort(mid+1 , end);
+    merge(start , end);
+}
+int main(void){
+#ifdef HELL_JUDGE
+    freopen("input","r",stdin);
+    freopen("output","w",stdout);
+    freopen("error","w",stderr);
+#endif 
+    int t; scanf("%d",&t);
+    while(t--){
+         counter = 0;
+         int n; scanf("%d",&n);
+         for(int i=0;i<n;++i){
+             scanf("%d",&arr[i]);
+         }
+         mergeSort(0 , n-1);
+         printf("%lld\n",counter);
+    }
+    return 0;
+}
